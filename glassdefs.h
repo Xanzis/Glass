1944b27b66c9a45be8de4f0bdc24a2d1c1db05e2
Xanzis <xander@hughes-family.org>
2020-10-19 19:24:14 -0400

    tokenization and env init written but not tested

--- glassdefs.h
+++ glassdefs.h
@@ -4,13 +4,15 @@
 // instance type labels
 
 enum val_type {NONE, FUNC, NUMB, NAME, STNG, CMDS};
-enum class_type {NOTCLASS, BUILTIN, USERDEF};
+enum token_type {NO_TOKEN, ASCII, NAME_IDX, NUMBER, STNG_IDX, STCK_IDX};
 
 typedef struct val val;
-typedef struct named_val named_val;
-typedef struct class class;
-typedef struct instance instance;
 typedef struct v_list v_list;
+typedef struct glass_env glass_env;
+typedef struct tok tok;
+
+int find_name(char** all_n, char* n);
+int add_name(char** all_n, char* n);
 
 struct val {
 	enum val_type type;
@@ -26,32 +28,57 @@ struct val {
 	};
 };
 
-struct named_val {
-	char* name;
-	val   v;
-};
-
-
-struct class {
-	char*      name;
-	val        init_func;
-	val        dest_func;
-	named_val* fncs;
-
-	named_val* vars;
-};
-
-struct instance {
-	enum class_type type;
-	class*     clss;
-	named_val* vars;
-};
-
 // data structure for the stack
 struct v_list {
 	int        last_i;
 	size_t     alloc;
 	val*       vs;
 };
+
+// data structure for parser output
+
+struct token_t {
+	enum token_type type;
+	int data;
+};
+
+struct glass_env {
+	char** names;    // all names defined in the program (for debug purposes)
+	int* c_lookup;   // c_lookup[i] = n means the ith class has name n (in the name array)
+	int** f_lookup;  // f_lookup[c][i] = n means the number ith function of the cth class has name n
+	int***  f_locs;  // f_locs[c][f] points to the first token of the fth function of cth class
+	token_t* tokens; // array of tokens forming the program. 
+
+	char** strings;  // array of all string literals used in program
+};
+
+int find_name(char** all_n, char* n) {
+	// finds name n (null-terminated) in all_n
+	// returns -1 on failure
+	for (int i = 0; all_n[i]; i++) {
+		if (!strcmp(all_n[i], n)) return i;
+	}
+	return -1;
+}
+
+int add_name(char** all_n, char* n) {
+	// adds the name to the end of all_n, returns the place it was added
+	// checks if name is already there, if so returns existing location
+	// allocates new memory for the name, sets all_n[i] pointer
+	// returns -1 on failure
+	int loc = find_name(all_n, n);
+	if (loc >= 0) return loc;
+	int i = 0;
+	do {
+		if (!all_n[i]) {
+			// allocate space for the new name and 
+			all_n[i] = (char*) malloc(strlen(n) * sizeof (char));
+			strcpy(all_n[i], n);
+			return i;
+		}
+		i++
+	} while (i < MAX_NAMES)
+	return -1;
+}
 
 #endif
--- parser.h
+++ parser.h
@@ -8,6 +8,14 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#define MAX_NAMES 256
+#define MAX_CLASSES 256
+#define MAX_FUNCS 256
+#define MAX_LITERALS 256
+#define MAX_PROGRAM 1024
+
+#define STD_LIBS 5 // number of standard classes
+
 void parse_error(char* err_text) {
 	fprintf(stderr, "Error in parser.h: %s\n", err_text);
 	exit(1);
@@ -35,20 +43,120 @@ void check_par(char* start, char a, char b, char end) {
 }
 
 void read_name(char* buff, char* start, int lim) {
-	// copies an [a-Z] name (length < lim-1) in parens into buff
+	// copies a name (length < lim-1) possibly in parens into buff
 	if (*start == '(') {
 		start++;
 		for (int i = 0; i != ')'; i++) {
-			if (!isalpha(start[i])) parse_error("bad name");
 			if (i >= (lim - 2)) parse_error("name too long");
 			buff[i] = start[i];
 			buff[i + 1] = 0;
 		}
 	}
 	else {
-		if (!isalpha(*start)) parse_error("bad name");
 		buff[0] = *start;
 		buff[1] = 0;
+	}
+}
+
+char* read_string(char* start) {
+	// parses a string in ""s, allocates new memory for it
+	// start shoud point to first delimiter
+	char tmp[64] = {0};
+	start++;
+	for (int i = 0; start[i] != '"'; i++) {
+		tmp[i] = start[i];
+	}
+	char* res = (char*) malloc(strlen(tmp) + 1);
+	strpcy(res, tmp);
+	return res;
+}
+
+int read_number(char* start) {
+	// parses a number in <>s or ()s
+	// start should point to first delimiter
+	char tmp[64] = {0};
+	start++;
+	for (int i = 0; (start[i] != ')') && (start[i] != '>'); i++) {
+		tmp[i] = start[i];
+	}
+	return atoi(tmp);
+}
+
+token_t make_token(glass_env* env, char* start) {
+	// take pointed-to string and tokenize the most immediate chunk
+	// most characters are directly 
+	// this function assumes any syntax problems have been caught by now
+	//   -- particularly paren mismatches
+	token_t res;
+	char name_buff[64];
+	switch (*start) {
+		case '(':
+			if (isalpha(start[1])) {
+				// it's a name in parens
+				read_name(name_buff, start, 64);
+				int name_idx = add_name(env->names, name_buff);
+				if (name_idx < 0) parse_error("couldn't add name in make_token");
+				res.type = NAME_IDX;
+				res.data = name_idx;
+			}
+			else if (isnum(start[1])) {
+				// it's a number in parens - a stack retrieve
+				res.type = STCK_IDX;
+				res.data = read_number(start);
+				if (res.data < 0) parse_error("error reading number");
+			}
+			else parse_error("bad character in parens");
+			break;
+		case '<':
+			// it's a number in <>s - a number literal
+			res.type = NUMBER;
+			res.data = read_number(start);
+			if (res.data < 0) parse_error("error reading number");
+			break;
+		case '"':
+			// it's a string - allocate space, add to env->strings, fill out index
+			char* new_str = read_string(start);
+			int str_idx = 0;
+			while (env->strings[str_idx] && str_idx < MAX_LITERALS) str_idx++;
+			if (str_idx >= MAX_LITERALS) parse_error("MAX_LITERALS exceeded");
+			env->strings[str_idx] = new_str;
+			res.type = STNG_IDX;
+			res.data = str_idx;
+			break;
+		default:
+		if (isalpha(*start)) {
+			// single-character name
+			// add the name to the name list and set token accordingly
+			read_name(name_buff, start, 64);
+			int name_idx = add_name(env->names, name_buff);
+			if (name_idx < 0) parse_error("couldn't add name in make_token");
+			res.type = NAME_IDX;
+			res.data = name_idx;
+		}
+		else {
+			// generic ascii - could be command, brackets, whatever
+			// set the type and value
+			res.type = ASCII;
+			res.data = (int) *start;
+		}
+	}
+	return res;
+}
+
+char* end_of_token(char* start) {
+	// returns a pointer to the character after a chunk
+	switch (*start) {
+		case '<':
+			while(start++ != '>');
+			return start;
+		case '"':
+			while(start++ != '"');
+			return start;
+		case '(':
+			while(start++ != ')');
+			return start;
+		default:
+		return start + 1;
 	}
 }
 
@@ -97,88 +205,110 @@ char* read_clean(char* loc) {
 	return res;
 }
 
-named_val build_func(char* start) {
-	// given a pointer to the opening [ of a function, parses the function and returns it
-	named_val res;
-	res.v.type = CMDS;
+void alloc_env(glass_env* env) {
+	// allocate all the various arrays and nested arrays in an env
+	// initialize everything to 0
 
-	// quick pass to check that parens are intact
-	check_par(start, '(', ')', ']');
-	// also check that <>s match
-	check_par(start, '<', '>', ']');
+	env->names = (char**) malloc(MAX_NAMES * sizeof (char*));
+	memset(env->names, 0, MAX_NAMES * sizeof (char));
+	env->names[0] = "~";
 
-	assert(*start = '[');
-	start++;
+	env->c_lookup = (int*) malloc(MAX_CLASSES * sizeof (int));
+	memset(env->c_lookup, 0, MAX_CLASSES * sizeof (int));
 
-	res.name = (char*) malloc(64);
-	read_name(res.name, start, 64);
-
-	// Move start to after the name
-	if (*start == '(') while(!(*(start++) == ')'));
-	else start++;
-
-	int lim = 1000;
-	res.v.cmds = (char*) malloc(lim);
-	check_ptr(res.v.cmds);
-	for (int i = 0; start[i] != ']'; i++) {
-		if (i >= (lim - 3)) {
-			lim += 1000;
-			res.v.cmds = (char*) realloc(res.v.cmds, lim);
-			check_ptr(res.v.cmds);
-		}
-		res.v.cmds[i] = start[i];
-		res.v.cmds[i+1] = 0;
-	}
-
-	return res;
-}
-
-class build_class(char* start) {
-	// given a pointer to the opening { of a class, parses the class and returns it
-	class res;
-	res.fncs = NULL;
-	res.vars = NULL;
-	res.init_func.type = NONE;
-	res.dest_func.type = NONE;
-	int n_funcs = 0;
-
-	// quick pass to check that brackets are intact
-	assert(*start == '{');
-	check_par(start, '[', ']', '}');
-	start++;
-
-	res.name = (char*) malloc(64);
-	read_name(res.name, start, 64);
-
-	// Move start to after the name
-	if (*start == '(') while(!(*(start++) == ')'));
-	else start++;
-
-	named_val temp;
-	for (int i = 0; start[i] != '}'; i++) {
-		if (start[i] == '[') {
-			temp = build_func(start + i);
-
-			if (!strcmp(temp.name, "c__"))      res.init_func = temp.v;
-			else if (!strcmp(temp.name, "d__")) res.dest_func = temp.v;
-			else {
-				n_funcs++;
-				res.fncs = (named_val*) realloc(res.fncs, (n_funcs + 1) * sizeof (named_val));
-				check_ptr(res.fncs);
-				res.fncs[n_funcs - 1] = temp;
-			}
+	env->f_lookup = (int**) malloc(MAX_CLASSES * sizeof (int*));
+	for (int i = 0; i < MAX_CLASSES; i++) {
+		env->f_lookup[i] = (int*) malloc(MAX_FUNCS * sizeof (int));
+		for (int j = 0; j < MAX_FUNCS; j++) {
+			env->f_lookup[i][j] = -1;
 		}
 	}
 
-	res.fncs[n_funcs].v.type = NONE;
+	env->f_locs = (int***) malloc(MAX_CLASSES * sizeof (int**));
+	for (int i = 0; i < MAX_CLASSES; i++) {
+		env->f_locs[i] = (int**) malloc(MAX_FUNCS * sizeof (int*));
+		memset(env->f_locs[i], 0, MAX_FUNCS * sizeof (int*));
+	}
 
-	return res;
+	env->tokens = (token_t*) malloc(MAX_PROGRAM * sizeof (token_t));
+	memset(env->c_lookup, 0, MAX_PROGRAM * sizeof (token_t));
 }
 
-class* parse_file(char* filename) {
-	// reads in a file, returns an array of the defined classes
-	class* res = NULL;
-	int n_classes = 0;
+void add_class(glass_env* env, char* name) {
+	// adds a class to the env c_lookup
+	if (find_name(env->names, name) >= 0) {
+		parse_error("cannot overwrite existing class");
+	}
+	// seek the end of class_lookup
+	int i = 0;
+	while (env->c_lookup[i]) i++;
+
+	env->c_lookup[i] = add_name(env->names, name);
+	if (env->c_lookup[i] < 0) parse_error("couldn't add class name");
+}
+
+void add_class_func(glass_env* env, char* c_name, char* f_name) {
+	// adds a function to the env f_lookup
+	int c_name_i = find_name(env->names, c_name);
+	// this should never happen
+	if (c_name_i < 0) parse_error("no such class name");
+
+	// get the class' index in f_lookup
+	int c_idx = 0;
+	while(env->c_lookup[c_idx]) {
+		if (env->c_lookup[c_idx] == c_name_i) break;
+		c_idx++;
+	}
+	// this should never happen
+	if (!c_idx) parse_error("couldn't find class name");
+
+	// pull out the relevant entry in f_lookup
+	int* f_name_idxs = env->f_lookup[c_idx];
+	int empty_i = 0;
+	while(empty_i && empty_i < MAX_FUNCS) empty_i++;
+	if (empty_i >= MAX_FUNCS) parse_error("MAX_FUNCS exceeded");
+
+	// add the function name, fill out entry
+	int f_name_i = add_name(env->names, f_name);
+	if (f_name_i < 0) parse_error("could not add function name");
+	f_name_idxs[f_name_i] = f_name_i;
+}
+
+void init_env(glass_env* env) {
+	// allocate subarrays of an env and fill out the standard library
+	
+	alloc_env(env);
+
+	// build out the standard classes and functions
+	// establish 
+	char* standard_names[] = {"A", "S", "V", "O", "I", "M", NULL};
+
+	// establish orders of the standard 
+	char* std_A_funcs[] = {"a", "s", "m", "d", "mod", "f", "e", "ne", "lt", "le", "gt", "ge", NULL};
+	char* std_S_funcs[] = {"l", "i", "si", "a", "d", "e", "ns", "sn", NULL};
+	char* std_V_funcs[] = {"n", "d", NULL};
+	char* std_O_funcs[] = {"o", "on", NULL};
+	char* std_I_funcs[] = {"l", "c", "e", NULL};
+
+	char*** stds = {std_A_funcs, std_S_funcs, std_V_funcs, std_O_funcs, std_I_funcs, NULL};
+
+	// add the standard class names and their functions
+	for (int i = 0; standard_names[i]; i++) {
+		add_class(env, standard_names[i]);
+	}
+	for (int i = 0; stds[i]; i++) {
+		for (int j = 0; stds[i][j]; j++) {
+			add_class_func(env, standard_names[i], stds[i][j]);
+		}
+	}
+}
+
+glass_env parse_file(char* filename) {
+	// reads in a file, returns parsed and tokenized data to the interpreter
+	// max numbers of names, classes etc. are fixed for now.
+	glass_env res;
+	// intialize the name and lookup arrays with the standard classes and functions
+	init_env(&res);
 
 	char* file = read_clean(filename);
 	char* start = file;
@@ -186,14 +316,6 @@ class* parse_file(char* filename) {
 	//check for matching braces
 	check_par(start, '{', '}', '\0');
 	start--; //TODO: hack
-	while(*(++start)) {
-		if (*start == '{') {
-			n_classes++;
-			res = (class*) realloc(res, (n_classes + 1) * sizeof (class));
-			check_ptr(res);
-			res[n_classes - 1] = build_class(start);
-		}
-	}
 
 	res[n_classes].name = NULL;
 
